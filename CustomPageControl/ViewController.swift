ffc1f4c74963cd00a73776e6e7408ef97bf2f90c
ahmad farizy <ahmad@code.id>
2019-03-20 14:30:56 +0700

    Initial Commit

---
+++ CustomPageControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomPageControl
+//
+//  Created by MAC on 20/03/19.
+//  Copyright © 2019 frzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
